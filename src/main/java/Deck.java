009b4fca42ef96edeeb1bf29e71ef21ba9ffa0bf
ipahenderson <ipahenderson@gmail.com>
2017-12-06 12:11:11 +0000

    Deck class made and deckStartsEmpty and ceckHas53Cards test passed

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,2 +1,36 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Deck {
+
+    private ArrayList<Card> cards;
+
+
+    public Deck(){
+        this.cards = new ArrayList<>();
+    }
+
+
+    public int deckSize(){
+        return this.cards.size();
+    }
+
+    public void populateDeck(){
+        int i = 0;
+        for(SuitType suit : SuitType.values()){
+            for(ValueType value : ValueType.values()){
+                cards.add(new Card(suit, value));
+                i++;
+            }
+
+        }
+    }
+
+
+
+
+
+
+
+
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -1,2 +1,29 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class DeckTest {
+
+    Deck deck;
+
+
+    @Before
+    public void before(){
+        deck = new Deck();
+    }
+
+    @Test
+    public void deckStartsEmpty(){
+        assertEquals(0, deck.deckSize());
+    }
+
+
+    @Test
+    public void deckHas52Cards(){
+        deck.populateDeck();
+        assertEquals(52, deck.deckSize());
+    }
+
 }
